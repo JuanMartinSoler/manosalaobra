4496047d58bc8e0217cd99ad0a1d0a84225e3349
jsoler <solerjuanmartin@gmail.com>
2022-03-21 12:49:03 -0300

    App ajustada a manos a la obra III (sin terminar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskItemComponent
   ],
   imports: [
     BrowserModule
