0b97bc26a51db3393b16f9124606cfdc82c6975f
jsoler <solerjuanmartin@gmail.com>
2022-03-25 15:30:14 -0300

    otro retoques scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'manosalaobra';
+  title = 'ToDoList';
   nombre = "juanchi";
   tareas = ["Task N","Task N-1","Completed task N-2","Task k","Task 2","Completed task 1"];
-
   input:string = "";
-//Metodo para agregar tareas a la lista 
-creartareas(nuevatarea:any){
-  if (!nuevatarea) return
-  this.tareas.unshift(nuevatarea);
-}
-// Metodo para borrar tareas de la lista
-borrartarea(tarea:any){
-  var i = this.tareas.indexOf(tarea);
-  if (i !== -1) {
-    this.tareas.splice(i,1);
-  }
-} 
   
+  //Metodo para agregar tareas a la lista 
+  creartareas(nuevatarea:any){
+    if (!nuevatarea) return
+    this.tareas.unshift(nuevatarea);
+  }
+
+  // Metodo para borrar tareas de la lista
+  borrartarea(tarea:any){
+    var i = this.tareas.indexOf(tarea);
+    if (i !== -1) {
+      this.tareas.splice(i,1);
+    }
+  }   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    NewTaskComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule
